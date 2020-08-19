ecf5c09c2a21149cb71648e89e287c01dae65504
Nick Matvyeyev <mnasoft@gmail.com>
2020-08-19 08:15:31 +0300

    Wed Aug 19 05:15:31 UTC 2020

--- mnas-dns.lisp
+++ mnas-dns.lisp
@@ -1,9 +1,11 @@
 ;;;; mnas-dns.lisp
 
 (in-package #:mnas-dns)
+(annot:enable-annot-syntax)
 
 ;;; "mnas-dns" goes here. Hacks and glory await!
 
+@export
 (defun ip-by-name (name)
   "Выполняет определение IP-адреса ПК по его имени;
 Возвращает:
@@ -24,3 +26,65 @@
     (SB-BSD-SOCKETS:SOCKET-ERROR (condition) (values nil condition))     
     (ERROR (condition) (values nil condition))
     (:no-error (varN-1 ) (list (format nil "~{~A~^.~}" varN-1)))))
+
+@export
+(defparameter *nginx-conf-dir* "D:/PRG/nginx-bin/conf")
+
+@export
+@annot.doc:doc
+"@b(Описание:) функция @b(nginx-by-ip-allow&deny-all) записывает в файл с 
+именем fname разрешения на доступ к серверу, работающему под управлением nginx.
+
+Для того, чтобы сервер воспринял это необходимо включить директиву  include
+в контекст http файла nginx.conf.
+
+ @b(Пример включения директивы include в контекст http файла nginx.conf:)
+@begin[lang=nginx](code)
+http {
+    include       server.blacklist;
+    ...
+    }
+@end(code)
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (nginx-by-ip-allow&deny-all '(\"192.168.0.100\"))
+@end(code)
+ "
+(defun nginx-by-ip-allow&deny-all
+    (ip-lst
+     &optional
+       (fname (concatenate 'string *nginx-conf-dir* "/" "server.blacklist")))
+  (with-open-file
+      (os fname :direction :output :if-exists :supersede :external-format :utf8)
+    (format os "~{allow ~A;~%~}~%deny all;" ip-lst)
+    (format nil "См. файл ~A~%~{allow ~A;~%~}~%deny all;" fname ip-lst)))
+
+@export
+@annot.doc:doc
+"@b(Описание:) функция @b(nginx-by-ip-allow&deny-all) записывает в файл с 
+именем fname разрешения на доступ к серверу, работающему под управлением nginx.
+
+Для того, чтобы сервер воспринял это необходимо включить директиву  include
+в контекст http файла nginx.conf.
+
+ @b(Пример включения директивы include в контекст http файла nginx.conf:)
+@begin[lang=nginx](code)
+http {
+    include       server.blacklist;
+    ...
+    }
+@end(code)
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (nginx-by-ip-allow&deny-all '(\"n118383\"))
+@end(code)
+ "
+(defun nginx-by-compnames-allow&deny-all
+    (compnames
+     &optional
+       (fname (concatenate 'string *nginx-conf-dir* "/" "server.blacklist")))
+  (nginx-by-ip-allow&deny-all
+   (mapcan #'(lambda (el) (mnas-dns:ip-by-name el)) compnames)
+   fname))
